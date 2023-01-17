4e2b511222c7be8c82270a125491252a26b4e903
Andrew Healey <healeycodes@gmail.com>
2023-01-17 23:18:01 +0000

    Add benchmark program

---
+++ benchmark.py
@@ -0,0 +1,19 @@
+import time
+from interpreter import interpret
+
+program = """
+for (i = 0; i < 20; i = i + 1)
+  # gets called 35400 times
+  fun fib(x)
+    if (x == 0 or x == 1)
+        return x;
+    fi
+    return fib(x - 1) + fib(x - 2);
+  nuf
+  fib(i);
+rof
+"""
+
+t = time.time()
+interpret(program, opts={"debug": False})
+print(time.time() - t)
